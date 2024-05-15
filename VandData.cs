25a824fe63b8f4d263578433e51fd2151332aa5a
DanielTed23 <daniel2003outlook.dk>
2024-05-15 08:59:05 +0200

    Add project files.

---
+++ VandData.cs
@@ -0,0 +1,16 @@
+﻿namespace VandQuizJson
+{
+    public class VandData
+    {
+        public string sporgsmol { get; set; }
+        public List<string>? svarmulighed1{ get; set; }
+
+        public int? korrektsvar { get; set; }
+
+        public string?  infoText { get; set; }
+
+
+
+
+    }
+}
