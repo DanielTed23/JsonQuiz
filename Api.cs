88762abefddace000b826f27bba1755bdf931262
DanielTed23 <daniel2003outlook.dk>
2024-05-24 10:35:56 +0200

    lavet en ny class

--- Api.cs
+++ Api.cs
@@ -1,9 +1,10 @@
 ﻿using Newtonsoft.Json;
 using RestSharp;
 
+
 namespace VandQuizJson
 {
-    internal class Api
+    internal class Api : ApiRC
     {
         public void Apirun()
         {
@@ -20,12 +21,12 @@ namespace VandQuizJson
                 int i = 0;
                 foreach (var answer in question.answers)
                 {
-                    
+
                     if (answer.Value is not null)
                     {
-                       
-                        Console.WriteLine(abc[i]+") " + answer.Value);
-                        
+
+                        Console.WriteLine(abc[i] + ") " + answer.Value);
+
                     }
                     i++;
                 }
@@ -33,7 +34,7 @@ namespace VandQuizJson
                 // Tager input fra bruger, og ligger det i en variable
                 Console.WriteLine("Indtast dit svar");
                 string svar = Console.ReadLine();
-                
+
 
                 // Sammensætter svar til en ny string, som bliver kontrollet, om svaret er korrekt / true
                 string svarString = "answer_" + svar + "_correct";
@@ -48,34 +49,12 @@ namespace VandQuizJson
                 }
 
             }
+
+            
         }
 
-
-        private static string GetApiResponse(string difficulty = "Easy", string limit = "10", string category = "Code")
-        {
-            // Set the url for the API that we want to call
-            RestClient client = new("https://quizapi.io/api/v1/questions");
-            // Create a request to the API
-            RestRequest request = new();
-
-            // Add parameters to the request
-
-            request.AddParameter("apiKey", "5WSuwStMLRtsTBirMAQP9ZJhU0zpIFwisu3Ok4El"); // Add my API-Key
-            request.AddParameter("limit", limit); // Set the limit of questions
-            request.AddParameter("difficulty", difficulty); // Set the difficulty
-            request.AddParameter("category", category); // Set the category
-
-            return client.Execute(request).Content; // Return the response content from the API
-        }
-
-
-
-        public static List<Format> ConvertApi()
-        {
-            // Convert the response to a list of Format objects by deserializing the JSON-response
-            return JsonConvert.DeserializeObject<List<Format>>(GetApiResponse(/*GetDifficulty(), GetLimit(), GetCategory()*/));
-        }
     }
+
 }
 
 
---
+++ ApiRC.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VandQuizJson
+{
+    internal class ApiRC
+    {
+        private static string GetApiResponse(string difficulty = "Easy", string limit = "10", string category = "Code")
+        {
+            // Set the url for the API that we want to call
+            RestClient client = new("https://quizapi.io/api/v1/questions");
+            // Create a request to the API
+            RestRequest request = new();
+
+            // Add parameters to the request
+
+            request.AddParameter("apiKey", "5WSuwStMLRtsTBirMAQP9ZJhU0zpIFwisu3Ok4El"); // Add my API-Key
+            request.AddParameter("limit", limit); // Set the limit of questions
+            request.AddParameter("difficulty", difficulty); // Set the difficulty
+            request.AddParameter("category", category); // Set the category
+
+            return client.Execute(request).Content; // Return the response content from the API
+        }
+
+
+
+        public static List<Format> ConvertApi()
+        {
+            // Convert the response to a list of Format objects by deserializing the JSON-response
+            return JsonConvert.DeserializeObject<List<Format>>(GetApiResponse(/*GetDifficulty(), GetLimit(), GetCategory()*/));
+        }
+    }
+}
